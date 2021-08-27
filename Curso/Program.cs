de51d524c9ed15d924b25c2879b75438f4473dc3
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-08-27 19:53:30 -0300

    Initial commit

---
+++ Curso/Program.cs
@@ -0,0 +1,28 @@
+﻿using Curso.Entities;
+using Curso.Entities.Enums;
+using System;
+
+namespace Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                ID = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(os);
+        }
+    }
+}
