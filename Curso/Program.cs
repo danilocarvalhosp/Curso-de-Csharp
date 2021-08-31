c1044b8bf4424cf8d3690a9c42b2029d4e494a59
Danilo Carvalho <danilocarvalhosp@gmail.com>
2021-08-31 20:13:31 -0300

    Herança
    Exercício 126 - Curso Udemy Nélio Alves

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Curso.Entities;
 using System;
 using System.Globalization;
-using Curso.Entities.Enums;
 
 namespace Curso
 {
@@ -9,44 +8,11 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter client data:");
-            Console.Write("Name: ");
-            string clientName = Console.ReadLine();
-            Console.Write("Email: ");
-            string clientEmail = Console.ReadLine();
-            Console.Write("Birth date (DD/MM/YYYY): ");
-            DateTime birthDate = DateTime.Parse(Console.ReadLine());
-            Console.WriteLine("Enter order data:");
-            Console.Write("Status: ");
-            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.0, 500.0);
 
-            Client client = new Client(clientName, clientEmail, birthDate);
-            Order order = new Order(DateTime.Now, status, client);
+            Console.WriteLine(account.Balance);
 
-            Console.Write("How many items to this order? ");
-            int itens = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i<=itens; i++)
-            {
-                Console.WriteLine($"Enter #{i} item data:");
-                Console.Write("Product name: ");
-                string productName = Console.ReadLine();
-                Console.Write("Product price: ");
-                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                Product product = new Product(productName, price);
-
-                Console.Write("Quantity: ");
-                int quantity = int.Parse(Console.ReadLine());
-
-                OrderItem orderItem = new OrderItem(quantity, price, product);
-
-                order.AddItem(orderItem);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("ORDER SUMMARY:");
-            Console.WriteLine(order);
+            // account.Balance = 200;
         }
     }
 }
